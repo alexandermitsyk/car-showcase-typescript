4b8a8b7c2ba9f76e96b431290b4f74204e300213
HOME\mitsy <mitsyksandor@gmail.com>
2023-06-12 20:09:50 +0200

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,21 @@
+import CustomButton from './CustomButton';
+import Hero from './Hero';
+import NavBar from './NavBar';
+import Footer from './Footer';
+import CustomFilter from './CustomFilter';
+import SearchBar from './SearchBar';
+import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
+import ShowMore from './ShowMore';
+
+export {
+    Hero,
+    CustomButton,
+    NavBar,
+    Footer,
+    SearchManufacturer,
+    SearchBar,
+    CustomFilter,
+    CarCard,
+    ShowMore,
+};
